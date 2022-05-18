afa605ffd1bcddad54fe5ba21e253591652788bb
duxx788 <50477494+duxx788@users.noreply.github.com>
2022-05-18 23:37:25 +0800

    modify

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import os
 
-
 filepath = '.\\'
 
 
 def dfs(path):
     filenames = os.listdir(path)
-    #print(filenames)
+    # print(filenames)
     for filename in filenames:
-        d = os.path.join(path,filename)
-        #print(d)
+        d = os.path.join(path, filename)
+        # print(d)
         if os.path.isdir(d):
             dfs(d)
             continue
         if d.find('test.txt') != -1:
-            with open(d,'r') as f:
+            with open(d, 'r') as f:
                 filedata = f.read()
-            filedata = filedata.replace(" dsm"," vendor_dsm")
-            with open(d,'w') as f:
+            filedata = filedata.replace(" dsm", " vendor_dsm")
+            with open(d, 'w') as f:
                 f.write(filedata)
+
 
 dfs(filepath)
